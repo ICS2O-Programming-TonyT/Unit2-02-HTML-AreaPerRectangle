5c44dcf39907a1f01b3e16b6426c29490788f6e0
ErrroTwo <80226295+ErrorThree@users.noreply.github.com>
2023-03-07 15:02:10 +0000

    e

--- js/script.js
+++ js/script.js
@@ -2,17 +2,21 @@ const length = 5;
 const width = 3;
 
 function calculateArea() {
+  const length = document.getElementById("Length").value;
+  const width = document.getElementById("Width").value;
 	const area = length * width;
 	alert(`The area of the rectangle is ${area} square meters.`);
 }
 
 function calculatePerimeter() {
+  const length = document.getElementById("Length").value;
+  const width = document.getElementById("Width").value;
 	const perimeter = 2 * (length + width);
 	alert(`The perimeter of the rectangle is ${perimeter} meters.`);
 }
 
-const areaBtn = document.getElementById("areaBtn");
-const perimeterBtn = document.getElementById("perimeterBtn");
+const areaBtn = document.getElementById("area-btn");
+const perimeterBtn = document.getElementById("perimeter-btn");
 
 areaBtn.addEventListener("click", calculateArea);
 perimeterBtn.addEventListener("click", calculatePerimeter);
